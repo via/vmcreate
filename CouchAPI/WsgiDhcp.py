c2f41f7c191279fdd55818470a4fec23a9aab9aa
Matthew Via <matthew.via@mailtrust.com>
2011-05-09 17:55:51 -0400

    Working on making code query couchdb.

--- CouchAPI/WsgiDhcp.py
+++ CouchAPI/WsgiDhcp.py
@@ -1,18 +1,35 @@
 from cgi import escape
 import re
+import couchdb
 
 
 class WsgiDhcp:
 
-  def __init__(self):
+  def __init__(self, servername):
+    self.couch = couchdb.Server(servername)
+    self.hostsdb = self.couch['hosts']
     pass
 
   def __call__(self, env, start_response):
+    if env['REQUEST_METHOD'] == 'GET':
+      return self.GET(env, start_response)
+    else: 
+      start_response('405 Bad Method', 
+          [('Content-Type', 'text/plain')])
+      return ["DHCP information is read only"]
+
+  def GET(self, env, start_response):
     mac = escape(env['PATH_INFO'][1:]).upper()
-    if (not re.match("([0-9A-F]{2}:){5}[0-9A-F]{2}", mac)):
+    if (not re.match("([0-9A-F]{2}:){5}[0-9A-F]{2}$", mac)):
       start_response('400 Invalid MAC Address', [('Content-Type', 'text/plain')])
       return ["Invalid MAC"]
 
-    
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [mac]
+
+    body = []
+    view = self.hostsdb.view('default/dhcp')
+    entries = view[[mac]:[mac, '()']]
+    for entry in view:
+      body.append(entry.id.encode('utf-8'))
+
+    return body
--- CouchAPI/main.py
+++ CouchAPI/main.py
@@ -6,7 +6,7 @@ from WsgiDhcp import  WsgiDhcp
 
 if __name__ == "__main__":
 
-  dhcpserver = WsgiDhcp()
+  dhcpserver = WsgiDhcp("http://pxe.testing.mlsrvr.com:5984")
 
   httpd = make_server('localhost', 8051, dhcpserver)
 
