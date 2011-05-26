363c6b3337c6c2134fbadde91c6e0357b30afd4b
Matthew Via <matthew.via@mailtrust.com>
2011-05-26 13:42:41 -0400

    Working version of WsgiDhcp

--- CouchAPI/WsgiDhcp.py
+++ CouchAPI/WsgiDhcp.py
@@ -1,5 +1,6 @@
 from cgi import escape
 import re
+import json
 import couchdb
 
 
@@ -8,7 +9,7 @@ class WsgiDhcp:
   def __init__(self, servername):
     self.couch = couchdb.Server(servername)
     self.hostsdb = self.couch['hosts']
-    pass
+    self.netsdb = self.couch['networks']
 
   def __call__(self, env, start_response):
     if env['REQUEST_METHOD'] == 'GET':
@@ -24,12 +25,36 @@ class WsgiDhcp:
       start_response('400 Invalid MAC Address', [('Content-Type', 'text/plain')])
       return ["Invalid MAC"]
 
-    start_response('200 OK', [('Content-Type', 'text/plain')])
 
     body = []
+    response = {}
     view = self.hostsdb.view('default/dhcp')
-    entries = view[[mac]:[mac, '()']]
-    for entry in view:
-      body.append(entry.id.encode('utf-8'))
+    entries = view[mac:mac]
+    if len(entries) > 1:
+      start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+      body.append("More than one host configured with this MAC. Please check"
+          "configuration")
+    elif len(entries) == 0:
+      start_response('404 Not Found', [('Content-Type', 'text/plain')])
+      body.append("No hosts configured with this MAC");
+    else:
+      host = self.hostsdb[entries.rows[0].id]
+      try:
+        network = self.netsdb[host["nics"][mac]["network"]]
+      except ResourceNotFound:
+        start_response('404 Not Found', [('Content-Type', 'text/plain')])
+        return "Network not found"
 
+
+      response["ip"] = host["nics"][mac]["ipv4"]
+      response["gateway"] = network["gateways"]
+      response["dns"] = network["nameservers"]
+      response["nextserver"] = network["nextserver"]
+      response["filename"] = "gpxelinux.0"
+
+
+      start_response('200 Ok', [('Content-Type', 'text/plain')])
+      body.append(json.dumps(response))
+    
     return body
+
